4c4e6da73b70822d220094f6f1bc1a315a420960
SystemOfPekalongan <77189271+SystemOfPekalongan@users.noreply.github.com>
2021-03-08 00:09:25 +0700

    Create duar.php

---
+++ duar.php
@@ -0,0 +1,90 @@
+<?php
+    /*
+    ==============================================================
+    mo ngapain bang?
+    mo recode ya?
+    yaelah bang tools sampah gini aja masih recode belajar lagi ya
+    ==============================================================
+    mending kembangin lg bang wordlist nya
+
+    */
+    $wd = array(
+        'setel/a.php','setelan/','plugins/','application','template','images/','js/','form_edit','bootstrap/','dist/','cbt'
+        ,'adminpannel/img/','adminpanel/login.php','systemadmin/','administrator/','admin1/','admin2/','admin3/','admin4/','admin5/','usuarios/',
+    'usuario/','administrator/','moderator/','webadmin/','adminarea/','bb-admin/','adminLogin/','admin_area/',
+    'panel-administracion/','instadmin/','memberadmin/','systemadmin','adm/','admin/account.php',
+    'admin/index.php','admin/login.php','admin/admin.php','admin/account.php','admin_area/admin.php',
+    'admin_area/login.php','siteadmin/login.php','siteadmin/index.php','siteadmin/login.html','admin/account.html',
+    'admin/index.html','admin/login.html','admin/admin.html','admin_area/index.php','bb-admin/index.php','bb-admin/login.php',
+    'bb-admin/admin.php','admin/home.php','admin_area/login.html','admin_area/index.html','admin/controlpanel.php','admin.php',
+    'admincp/index.asp','admincp/login.asp','admincp/index.html','admin/account.html','adminpanel.html','webadmin.html',
+    'webadmin/index.html','webadmin/admin.html','webadmin/login.html','admin/admin_login.html','admin_login.html',
+    'panel-administracion/login.html','admin/cp.php','cp.php','administrator/index.php','administrator/login.php',
+    'nsw/admin/login.php','webadmin/login.php','admin/admin_login.php','admin_login.php','administrator/account.php',
+    'administrator.php','admin_area/admin.html','pages/admin/admin-login.php','admin/admin-login.php','admin-login.php',
+    'bb-admin/index.html','bb-admin/login.html','acceso.php','bb-admin/admin.html','admin/home.html',
+    'login.php','modelsearch/login.php','moderator.php','moderator/login.php','moderator/admin.php','account.php',
+    'pages/admin/admin-login.html','admin/admin-login.html','admin-login.html','controlpanel.php','admincontrol.php',
+    'admin/adminLogin.html','adminLogin.html','admin/adminLogin.html','home.html','rcjakar/admin/login.php',
+    'adminarea/index.html','adminarea/admin.html','webadmin.php','webadmin/index.php','webadmin/admin.php',
+    'admin/controlpanel.html','admin.html','supermanasp','cp.html','adminpanel.php','moderator.html',
+    'administrator/index.html','administrator/login.html','user.html','administrator/account.html','administrator.html',
+    'login.html','modelsearch/login.html','moderator/login.html','adminarea/login.html','panel-administracion/index.html',
+    'panel-administracion/admin.html','modelsearch/index.html','modelsearch/admin.html','admincontrol/login.html',
+    'adm/index.html','adm.html','moderator/admin.html','user.php','account.html','controlpanel.html','admincontrol.html',
+    'panel-administracion/login.php','wp-login.php','adminLogin.php','admin/adminLogin.php','home.php','admin.php',
+    'adminarea/index.php','adminarea/admin.php','adminarea/login.php','panel-administracion/index.php',
+    'panel-administracion/admin.php','modelsearch/index.php','modelsearch/admin.php','admincontrol/login.php',
+    'adm/admloginuser.php','admloginuser.php','admin2.php','admin2/login.php','admin2/index.php','usuarios/login.php',
+    'adm/index.php','adm.php','affiliate.php','adm_auth.php','memberadmin.php','administratorlogin.php','admin.asp','admin/admin.asp',
+    'admin_area/admin.asp','admin_area/login.asp','admin_area/index.asp','bb-admin/index.asp','bb-admin/login.asp',
+    'bb-admin/admin.asp','pages/admin/admin-login.asp','admin/admin-login.asp','admin-login.asp','user.asp','webadmin/index.asp',
+    'webadmin/admin.asp','webadmin/login.asp','admin/admin_login.asp','admin_login.asp','panel-administracion/login.asp',
+    'adminLogin.asp','admin/adminLogin.asp','home.asp','adminarea/index.asp','adminarea/admin.asp','adminarea/login.asp',
+    'panel-administracion/index.asp','panel-administracion/admin.asp','modelsearch/index.asp','modelsearch/admin.asp',
+    'admincontrol/login.asp','adm/admloginuser.asp','admloginuser.asp','admin2/login.asp','admin2/index.asp','adm/index.asp',
+    'adm.asp','affiliate.asp','adm_auth.asp','memberadmin.asp','administratorlogin.asp','siteadmin/login.asp','siteadmin/index.asp',
+    'madspot.php','mad.php','404.php','anon.php','anonymous.php','shell.php','sh3ll.php',
+    'madspotshell.php','b374k.php','c100.php','priv8.php','private.php','cp.php','cpbrute.php','themes/404/404.php',
+    'templates/atomic/index.php','templates/beez5/index.php','hacked.php','r57.php','wso.php','WSO.php','wso24.php',
+    'wso26.php','wso404.php','sym.php','symsa2.php','sym3.php','whmcs.php','whmcskiller.php','cracker.php','1.php','2.php',
+    'sql.php','gaza.php','database.php','a.php','d.php','dz.php','cpanel.php','system.php','um3r.php','zone-h.php','c22.php',
+    'root.php','r00t.php','doom.php','dam.php','killer.php','user.php','wp-content/plugins/disqus-comment-system/disqus.php',
+    'cpn.php','shelled.php','uploader.php','up.php','xd.php','d00.php','h4xor.php','tmp/mad.php','tmp/1.php',
+    'wp-content/plugins/akismet/akismet.php','images/stories/w.php','w.php','downloads/dom.php','templates/ja-helio-farsi/index.php','wp-admin/m4d.php',
+    'd.php','wp-content/uploads/2021/01/12.php','wp-content/uploads/2021/02/12.php','wp-content/uploads/2021/03/12.php',
+    'wp-content/uploads/2021/04/12.php','wp-content/uploads/2021/05/12.php','wp-content/uploads/2021/06/12.php',
+    'wp-content/uploads/2021/07/12.php','wp-content/uploads/2021/08/12.php','wp-content/uploads/2021/09/12.php',
+    'wp-content/uploads/2021/10/12.php','wp-content/uploads/2021/11/12.php','wp-content/uploads/2021/12/12.php'
+    );
+
+    echo "
+
+----------------------------------------------------------------------
+		Mass Directory Scanner & shell checker
+		      Coded by root@x-krypt0n-x
+		  Copyright ©2020 SystemOfPekalongan
+----------------------------------------------------------------------
+";
+    echo "[*]This tools scan with methode get http response\n";
+    echo "[*]Scan speed depends on your own internet connection\n";
+	echo "[*]Use it wisely\n";
+    echo "Target : ";
+    $input_target = fopen("php://stdin","r"); 
+    $url = trim(fgets($input_target));
+
+
+    foreach ($wd as $key) {
+        $domain = $url.'/'.$key;
+
+        $cek = get_headers($domain);
+
+        if (preg_match('/200/', $cek[0])) {
+            echo "+===========================================================+\n";
+            echo "[*] found :D => ".$domain."\n";
+        }else{
+            echo "[x] not found or access forbidden :( => ".$domain."\n";
+        }
+
+    }
+?>
